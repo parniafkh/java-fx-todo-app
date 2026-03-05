da64d5c61d9e8ce49b2cc8edf5adb65e40877337
parniafkh <p.fakhri.2019@gmail.com>
2026-03-05 13:01:35 +0100

    feat: apply modern css styling to todo app

---
+++ src/main/java/com/parnia/todo/Launcher.java
@@ -0,0 +1,8 @@
+package com.parnia.todo;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Sie ruft einfach nur die Main-Klasse auf
+        Main.main(args);
+    }
+}
